068339531c1cea02cab18ec0729f5f985c54f79e
Banu Akkuş <akkusbanu92@gmail.com>
2018-10-15 11:21:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return array
 }
 
+//banu
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   array
