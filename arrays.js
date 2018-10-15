c71a873b524b0cb5f1632e0b753df7c6f1ddbe88
Banu Akkuş <akkusbanu92@gmail.com>
2018-10-15 11:20:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,20 +1,41 @@
-var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
-
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  return [array, ...element];
+  return [element, ...array]
 }
 
-function removeElementFromEndOfArray (array, element) {
-  return array.slice(0,array.lenght - 1)
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
 }
 
-function destructivelyRemoveElementFromEndOfArray (array, element) {
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
 
-function removeElementFromBeginningOfArray (array, element) {
-  array.pop()
-  return array
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
 }
